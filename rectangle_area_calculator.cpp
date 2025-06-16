c8bd8958f292ff643addfe49ab0b76d2a47bd817
MAHIK IRSHAD BHAT <bhatmahik.mb97@gmail.com>
2025-06-16 12:02:15 +0530

    Add files via upload

---
+++ rectangle_area_calculator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int main () {
+
+        // Simple rectangle area calculator which doesn`t take input 
+
+        int length = 20;
+        int width = 15;
+
+        cout << "The area of the given rectangle is " << length * width << " cm².";
+        
+        return 0;
+    }
